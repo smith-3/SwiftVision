6968a712005cb10ed32f0980b9982697758576ee
Manuel Smith Lazarte Alcocer <manuel.lazarte@jala.university>
2024-10-03 06:31:33 -0400

    Seguridad para la conexion con el archivo .env

--- alembic/env.py
+++ alembic/env.py
@@ -5,10 +5,14 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from database.database import Base
+from database.database import DATABASE_URL, Base
 from database.models import *
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+
+config = context.config
+config.set_main_option('sqlalchemy.url', DATABASE_URL)
+
 config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
